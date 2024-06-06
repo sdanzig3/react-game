88788ec1706f445b978436d1da0df1422ecd017a
Samuel Danziger <smdanziger@samuels-air.mynetworksettings.com>
2024-06-06 19:57:04 -0400

    Adding various graphics

--- be-gone-aliens/src/components/Canvas.jsx
+++ be-gone-aliens/src/components/Canvas.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
-import CannonBase from './CannonBase.jsx';
+import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
+import Heart from './Heart';
 
-const Canvas = () => {
+const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
     <svg
+    
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky />
       <Ground />
-      <CannonPipe rotation={45} />
+      <CannonPipe rotation={props.angle} />
       <CannonBase />
+      <CannonBall position={{x: 0, y: -100}}/>
+      <CurrentScore score={15} />
+      <FlyingObject position={{x: -150, y: -300}}/>
+      <FlyingObject position={{x: 150, y: -300}}/>
+      <Heart position={{x: -300, y: 35}} />
     </svg>
   );
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
 };
 
 export default Canvas;
